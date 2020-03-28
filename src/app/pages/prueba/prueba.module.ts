b6ecd8aad7602c9d499848b80978cfafc2348758
Joseba Merino <yosiunico@gmail.com>
2020-03-28 17:53:50 +0100

    Completado tutorial de arrastrar cosas

--- src/app/pages/prueba/prueba.module.ts
+++ src/app/pages/prueba/prueba.module.ts
@@ -8,12 +8,16 @@ import { PruebaPageRoutingModule } from './prueba-routing.module';
 
 import { PruebaPage } from './prueba.page';
 
+import { DragulaModule } from 'ng2-dragula';
+
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    PruebaPageRoutingModule
+    PruebaPageRoutingModule,
+    DragulaModule
   ],
   declarations: [PruebaPage]
 })
--- src/app/pages/prueba/prueba.page.ts
+++ src/app/pages/prueba/prueba.page.ts
@@ -1,20 +1,82 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DragulaService } from 'ng2-dragula';
+import { ToastController } from '@ionic/angular';
+import { ThrowStmt } from '@angular/compiler';
+
 @Component({
   selector: 'app-prueba',
   templateUrl: './prueba.page.html',
   styleUrls: ['./prueba.page.scss'],
 })
 export class PruebaPage implements OnInit {
+  q1 = [
+    { value: 'Buy Milk', color: 'primary' },
+    { value: 'Write new Post', color: 'primary' }
+  ];
+  q2 = [
+    { value: 'Schedule newsletter', color: 'secondary' },
+    { value: 'Find new Ionic Academy topics', color: 'secondary' }
+  ];
+  q3 = [
+    { value: 'Improve page performance', color: 'tertiary' },
+    { value: 'Clean the house', color: 'tertiary' }
+  ];
+  q4 = [
+    { value: 'Unimportant things', color: 'warning' },
+    { value: 'Watch Netflix', color: 'warning' }
+  ];
 
-  constructor() { }
+  todo = { value: '', color: '' };
+  selectedQuadrant = 'q1';
 
-  ngOnInit() {
+  constructor(private dragulaService: DragulaService, private toastController: ToastController) {
+    this.dragulaService.drag('bag').subscribe(
+      ({name, el , source}) => {
+        el.setAttribute('color', 'danger');
+      }
+    );
+
+    this.dragulaService.removeModel('bag').subscribe(
+      ({item}) => {
+        this.toastController.create({
+          message: `Removed: ${item.value}`,
+          duration: 2000
+        }).then(toast => toast.present());
+      }
+    );
+
+    this.dragulaService.dropModel('bag').subscribe(
+      ({item}) => {
+        item.color = 'success';
+      }
+    );
+
+    this.dragulaService.createGroup('bag', {
+      removeOnSpill: true
+    });
   }
 
+  addTodo() {
+    switch (this.selectedQuadrant) {
+      case 'q1':
+        this.todo.color = 'primary';
+        break;
+      case 'q2':
+        this.todo.color = 'secondary';
+        break;
+      case 'q3':
+        this.todo.color = 'tertiary';
+        break;
+      case 'q4':
+        this.todo.color = 'warning';
+        break;
+    }
+    this[this.selectedQuadrant].push(this.todo);
+    this.todo = { value: '', color: ''};
+  }
 
-  doReorder($event) {
-    
+  ngOnInit() {
   }
 
 }
